e3b4098256292dd27047d4e387d54bbe964f5644
Luka <lcvijan@gmail.com>
2024-02-26 02:44:31 +0100

    Added snippets and code completion

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,4 +1,4 @@
-return {
+return{
   {
     "williamboman/mason.nvim",
     config = function()
@@ -18,10 +18,28 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilites = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.pyright.setup({})
+      lspconfig.lua_ls.setup {
+        capabilites = capabilites,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'},
+            },
+          },
+        },
+      }
+      lspconfig.clangd.setup({
+        capabilites = capabilites
+      })
+      lspconfig.pyright.setup({
+        capabilites = capabilites
+      })
+
+      vim.keymap.set('n', '.', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
